a652dbdc26569688a14ea488032e1ebf4fb8db2d
Robert Heilmayr <rheilmayr@ucsb.edu>
2025-06-06 09:49:02 -0700

    Updating to new names, adding annotations

--- Scripts/full_analysis.R
+++ Scripts/full_analysis.R
@@ -26,22 +26,29 @@ set.seed(random_seed)
 
 
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-# Create directory to store outputs  ------
+# ------ Create directory to store outputs  ------
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 dir.create(here("Output"), showWarnings = FALSE)
 
 
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-# Run primary analysis  ------
+# ------ Run primary analysis  ------
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# Run primary analysis (Figure 6) and test robustness against precip changes (Figure S2)
 source("Scripts/Primary analysis/CounterfactualclimatechangeITRDB.R")
+
+# Generate marginal effects plot (Figure 5, Table S2)
 source("Scripts/Primary analysis/MarginalEffectsPlot.R")
 
 
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-# Run robustness tests  ------
+# ------ Run robustness tests  ------
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-source("Scripts/Robustness/Robustness_counterfactual_ITRDB.R")
-source("Scripts/Robustness/Robustness_DetrendingApproaches_ITRDB.R")
-source("Scripts/Robustness/SpatialAutocorrelationtTest.R")
-source("Scripts/Robustness/SpecChart_PIED.R")
+# Test robustness against alternate detrending approaches to counterfactual climate (Figure S1)
+source("Scripts/Robustness/Robustness_detrending.R")
+
+# Test robustness across multiple model specifications (Figure 7)
+source("Scripts/Robustness/Robustness_specChart.R")
+
+# Test for spatial autocorrelation (Table S3) (Warning: can take >1 hour to run)
+source("Scripts/Robustness/SpatialAutocorrelationTest.R")
